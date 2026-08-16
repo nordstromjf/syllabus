0dda5fb9b41f5c57b58557aa64927c9c933139c2
nordstromjf <jfirkins@linfield.edu>
2026-08-16 17:49:09 +0000

    Latest build deployed.

---
+++ discrete/lunr-pretext-search-index.js
@@ -0,0 +1,131 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "Discrete_syllabus-2",
+  "level": "1",
+  "url": "Discrete_syllabus-2.html",
+  "type": "Section",
+  "number": "",
+  "title": "Course Information",
+  "body": "Course Information  MATH 230 01  Fall 2026  MWF 10:00-10:50  Taylor 101   "
+},
+{
+  "id": "instructorinfo",
+  "level": "1",
+  "url": "instructorinfo.html",
+  "type": "Section",
+  "number": "",
+  "title": "Contact Information",
+  "body": " Contact Information    Instructor Jennifer Nordstrom  Office Taylor 205  Drop in Hours MTWR 1:30-3:00  But feel free to drop by any time.   Contact  jfirkins@linfield.edu    "
+},
+{
+  "id": "description230",
+  "level": "1",
+  "url": "description230.html",
+  "type": "Section",
+  "number": "",
+  "title": "Course Description",
+  "body": " Course Description  Topics in the general area of discrete mathematical structures including sets, logic, relations, functions, induction, matrices, basic enumeration, graphs, and Boolean algebra.  "
+},
+{
+  "id": "materials230",
+  "level": "1",
+  "url": "materials230.html",
+  "type": "Section",
+  "number": "",
+  "title": "Course Materials",
+  "body": " Course Materials  The text for this course is Discrete Mathematics, an Active Approach to Mathematical Reasoning by Jennifer Nordstrom. The text is a free open source text.  We will also be accessing the text through Runestone Academy . You will need to create a free account on Runestone. When you register on Runestone you will need to enroll in our course: linfield_discrete_fall26.  "
+},
+{
+  "id": "objectives230",
+  "level": "1",
+  "url": "objectives230.html",
+  "type": "Section",
+  "number": "",
+  "title": "Course Learning Outcomes",
+  "body": " Course Learning Outcomes     To introduce the student to the basic discrete mathematical structures and the theory and techniques associated with these structures.  To continue the student's growth in the use of mathematical language and the use of logic.  To improve the student's ability to write precise, valid, mathematical arguments.  To increase the student's confidence in working through mathematical challenges.  To continue the growth of the student's abilities in the appreciation and use of abstract reasoning.  To improve the student's problem-solving abilities.  To increase the student's mathematical maturity.    "
+},
+{
+  "id": "objectives230-2-1",
+  "level": "2",
+  "url": "objectives230.html#objectives230-2-1",
+  "type": "Objectives",
+  "number": "",
+  "title": "",
+  "body": "  To introduce the student to the basic discrete mathematical structures and the theory and techniques associated with these structures.  To continue the student's growth in the use of mathematical language and the use of logic.  To improve the student's ability to write precise, valid, mathematical arguments.  To increase the student's confidence in working through mathematical challenges.  To continue the growth of the student's abilities in the appreciation and use of abstract reasoning.  To improve the student's problem-solving abilities.  To increase the student's mathematical maturity.  "
+},
+{
+  "id": "schedule230",
+  "level": "1",
+  "url": "schedule230.html",
+  "type": "Section",
+  "number": "",
+  "title": "Daily Schedule",
+  "body": " Daily Schedule  Tentative daily schedule for Discrete Math     Week  Mon  Tues  Thur  Fri    1  1.1  1.2  1.3  2.1    2  No class  2.2  2.3  3.1    3  3.1  3.2  3.3  3.4    4  4.1  4.1  4.2  GOAL 1: Ch 1-3    5  4.3  4.3  4.4  4.5    6  4.6  4.6  Break  Break    7  5.1  5.2  5.3  5.3    8  5.4  5.4  5.5  5.6    9  6.1  6.1  6.2  GOAL 2: Ch 4-5    10  6.3  7.1  7.2  8.1    11  8.2  8.3  8.4  9.1\/9.2    12  9.3  GOAL 3: Ch 6-8  9.4  9.4    13  Break  Break  Break  Break    14  9.5  9.6  10.1  10.2    15  10.2  10.3  Cardinality  Cardinality    16  Reading Day   GOAL 4: 9-10; 10:30 am       "
+},
+{
+  "id": "community230",
+  "level": "1",
+  "url": "community230.html",
+  "type": "Section",
+  "number": "",
+  "title": "The Importance of Community",
+  "body": " The Importance of Community  Many aspects of this course are structured to build community. Although learning mathematics is often seen as an individual task, learning is more effective when you engage with others during the process. As a class we will determine the particular attributes we expect of each other in order to build our community. In addition, learning to work well in a community in which members bring different skills will better prepare you for life and work after college. Some ways you can help build a strong supportive community that improves learning are  Come to class ready to participate.  Help your team in a variety of ways: volunteer to write on the board, keep and organize notes for your group, ask questions, make suggestions, be positive and encouraging of others.  Come to office hours for help rather than relying on AI or other tools when you are unsure of the concepts. Your professor is part of the community, too. Building connections with faculty will help you not just learn Discrete Math, but also help connect you with other resources and opportunities.  Form study groups to work on homework together or review for quizzes. If you use AI to help you study, it is still better to do this in a group so you catch errors.  Don't be afraid to make mistakes. We all learn from each others' mistakes and misunderstandings. You are not expected to come to this class as a fully-formed mathematician. In fact, you aren't expected to leave this class as a fully-formed mathematician either. But if no one knows what mistakes you are making, no one can help you learn.    "
+},
+{
+  "id": "grading230",
+  "level": "1",
+  "url": "grading230.html",
+  "type": "Section",
+  "number": "",
+  "title": "Grading Information",
+  "body": " Grading Information  The following includes descriptions of the graded components of the course and the grading scale.   Homework  Homework will be due once a week on Fridays at 3 pm. Assignments are posted on the course website, accessible through Blackboard. Homework will be graded using a holistic grading system. Each section will be scored 0, 5 or 10. You will get a 5 for sets that are mostly complete, but missing some fundamental understanding. You will get a score of 10 if it is complete and mostly correct. You are permitted to not do problems that you feel 100% confident that you could do correctly if asked. Just indicate that it is a problem you are confident in, along with a brief reason. However, be prepared that I may ask you to explain it at a later date. It is generally not advisable to skip the proofs. I will provide feedback on all homework and any problems may be resubmitted to improve your score. All rewrites are due one week following the original due date. In addition, with each Homework set you can earn 4 points for answering the following reflection questions:  What is one problem or concept you feel you are still struggling with or feel the least sure about after completing this section?  What is one problem or concept that you feel you understand better after completing this section?  Did you get any help from anyone on this section? Describe the help you got.  Did you help anyone else with this homework? Describe the help you gave.   Homework extensions will be granted upon request. However, if a student has more than 3 overdue sections, they will receive a 0 in the most over-due sections. It is imperative that students stay caught up on the homework in order to keep up with new material.    How to turn in? Written assignments, generally not accepted electronically.  Late submission accepted? Yes. If no more than 3 missing sections.  Corrections allowed? Yes. Accepted within 1 week of original due date.     In Class Activites  Each section will have several activities that you will work on as a class or in small groups. All of the Activities should be done in the text on Runestone Academy . You will not be assessed on your specific answers to the activities, but the Activities and Check Your Understanding questions will be graded for completion. The Activities and Check Your Understanding questions will be due every Tuesday by 11:30 pm. These may be completed up to one week late. Generally you should complete the majority of this work in class. You are encouraged to have a laptop or tablet for submitting your work in class.   How to turn in? Completed in the textbook on Runestone.  Late submission accepted? Yes, within 1 week.    Daily Questions  At the start of class almost every day you will be asked to answer (and turn in your answer to) the Daily Questions. These will be a questions on previous material. The purpose of these is primarily diagnostic, not evaluative. They will be graded on a pass\/ no pass (P\/NP) basis. You may receive a NP on five Daily Questions with no penalty, after that, your will lose 3% of the Daily Question grade for each NP. There are absolutely no make-up questions. Excused absences arranged ahead of class will not count as NP; otherwise, missed Daily Question counts as a NP. It is important to arrive to class on time as these will only take a few minutes.  Group Oral Assessment of Learning (GOAL)  There will four group oral midterm quizzes. The class will be split into two groups. The quiz will be done in-class. The problems may be solved and presented by any subset of your group. The entire group will get the same grade, but students who contribute more may earn an additional 3% while students who are not active participants may lose 3%. It is expected that you study for the quiz, ideally as a group. Any student who is unable to attend class, for whatever reason, will have an opportunity to take an alternative oral quiz outside of class time. The purpose of these is to encourage teamwork and communication, both of which are important skills in mathematics.    GOAL 1 Friday, Sept. 25. Covers Ch 1-3.  GOAL 2 Friday, Oct. 30. Covers Ch 4-5.  GOAL 3 Tuesday, Nov. 17. Covers Ch 6-8.  GOAL 4 Wednesday, Dec. 16. Covers Ch 9-10.      What if I miss a GOAL? You can schedule an individual quiz outside of class time.       Grading System  Grades will be determined as follows:   Homework  25%    Activities and Check Your Understanding  25%    Group Oral Assessments of Learning  40%    Daily Questions  10%    Letter grades correspond to the following percentages:    A-, A  90-100%    B-, B, B+  80-89%    C-, C, C+  65-79%    D  55-64%     "
+},
+{
+  "id": "finalexam230",
+  "level": "1",
+  "url": "finalexam230.html",
+  "type": "Section",
+  "number": "",
+  "title": "Final Exam Time",
+  "body": " Final Exam Time  The final exam time for this class is scheduled for Wednesday, December 16 at 10:30 am .  We will be having the final Group Oral Assessment of Learning (GOAL) during the final exam time. You are required to be there to work with your group. If you are unable to attend due to a school sanctioned activity or illness, you will need to schedule an individual quiz before the end of finals week.  "
+},
+{
+  "id": "advising230",
+  "level": "1",
+  "url": "advising230.html",
+  "type": "Section",
+  "number": "",
+  "title": "Advising Information",
+  "body": " Advising Information  The prerequisite is MAT 170, Calculus I. This course is especially designed to serve the needs of students interested in computer science. Students majoring in areas outside the physical sciences such as economics and business will find the course of interest as well. This course carries credits toward a major or minor in mathematics and serves as good preparation for advanced courses such as abstract algebra, combinatorics, graph theory, and topology. It is also recommended for math majors interested in secondary education.  "
+},
+{
+  "id": "policies230",
+  "level": "1",
+  "url": "policies230.html",
+  "type": "Section",
+  "number": "",
+  "title": "Course Policies",
+  "body": " Course Policies  Cell Phones:  Cell phones must be off and put away during class. Laptop computers and tablets are encouraged, as we will be engaging with materials that are available electronically. However, please use them in ways that are focused on the course and the activities of the class.  Electronic Recording\/ Content Sharing: I may opt to record the classroom activities for instructional purposes and post them to the cloud. The electronic recording of classroom lectures, discussions, simulations, and other course-related activity is governed by Linfield's Classroom Recording Policy (Faculty Handbook, VII.26 and Student Policy Guide). Students do not have permission to record any Zoom meetings. Students do not have permission to distribute or share any recorded content from Zoom meetings.  Use of AI in Student-Generated Work: Students in this course are expected to avoid the use AI tools, such as Chat GPT, Gemini, Claude, and PhotoMath, to generate activity, quiz, or homework solutions. Any tools may only be used in a manner that contributes to understanding math, rather than avoiding the work necessary to deepen your understanding. Use of such tools, like any other academic work that is not entirely the student's own, must be cited. Work for a grade that is not primarily in the student's own words and properly cited, will be considered plagiarized. Note, many of the math tools that exist use techniques that are not part of this course.  If a violation is suspected, the first action will be a conversation about why your work may be read as AI generated. Work that is deemed not the student's own after this conversation will receive a 0 on the entire assignment\/ activity.  Some AI uses that are appropriate: helping summarize key concepts with additional examples, generating practice questions to study for a quiz or exam, helping create a study plan for balancing your workload. Uses that are not appropriate: generating homework or activity solutions, generating proofs for homework or activities, having AI correct your proofs or homework.   "
+},
+{
+  "id": "boilerplate",
+  "level": "1",
+  "url": "boilerplate.html",
+  "type": "Section",
+  "number": "",
+  "title": "Linfield Policies",
+  "body": " Linfield Policies   Disability Statement:  Students with disabilities are protected by the Americans with Disabilities Act and Section 504 of the Rehabilitation Act. If you are a student with a disability and feel you may require academic accommodations please contact Learning Support Services (LSS), as early as possible to request accommodation for your disability. The timeliness of your request will allow LSS to promptly arrange the details of your support. LSS is located in Melrose Hall 020 (503-883-2562), or LSS@linfield.edu . We also encourage students to communicate with faculty about their accommodations.  Academic Integrity Policy:  Linfield University operates under the assumption that all students are honest and ethical in the way they conduct their personal and scholastic lives. Academic work is evaluated on the assumption that the work presented is the student's own, unless designated otherwise. Anything less is unacceptable and is considered a violation of academic integrity. Furthermore, a breach of academic integrity will have concrete consequences that may include failing a particular course or even dismissal from the university. Violations of academic integrity include but are not limited to the following:  Cheating: Using or attempting to use unauthorized sources, materials, information, or study aids in any submitted academic work; changing answers after graded work has been returned; making unauthorized changes to an exam, quiz, or assignment.  Plagiarism: Submission of academic work that includes material copied or paraphrased from published or unpublished sources without proper documentation. This includes self-plagiarism, the submission of work created by the student for another class unless they receive consent from both instructors.  Fabrication: Deliberate falsification or invention of any information, data, or citation in academic work.  Facilitating Academic Dishonesty: Knowingly helping or attempting to help another to violate the university's policy on academic integrity.  For specific information about the use of AI, see .  Any form of academic dishonesty will result in a 0 on that assignment\/ quiz\/ exam. Additionally, academic dishonesty may result in a failing grade in the course. See the Linfield Academic Integrity Policy in the Linfield Catalog for information on the procedure to be used in dealing with academic dishonesty.  Sexual Misconduct and Relationship Violence and Title IX:  Linfield University faculty are committed to supporting students and fostering a campus environment free of sexual misconduct and relationship violence. If a student chooses to disclose to a faculty or staff member an experience related to sexual misconduct, sexual assault, domestic violence, dating violence, or stalking, all faculty and staff are obligated to report this disclosure to the Linfield Title IX Coordinator by emailing titleix@linfield.edu . Upon receipt of the report, the Title IX Coordinator will contact you to inform you of your rights and options and connect you with support services. If you would rather share information about these experiences with an employee who does not have these reporting responsibilities and can keep the information confidential, please visit confidential resources: linfield.edu\/safety\/sexual-misconduct\/reporting-options\/confidential.html .  For more information about your rights and reporting options at Linfield, including confidential reporting options, please visit Linfield's Know Your Options Page on the University website. Support services are offered to all Linfield students regardless of whether or not they report. Still have questions? Email titleix@linfield.edu .  Commitment to Diversity and Inclusion: Linfield University honors human rights and academic freedom, celebrates diverse cultures, fosters a climate of mutual respect, and promotes an inclusive environment that affirms the value of all persons. Dimensions of diversity can include sex, race, age, national origin, immigration status, ethnicity, gender identity and expression, intellectual and physical ability, sexual orientation, income, faith and non-faith perspectives, socio-economic class, political ideology, education, primary language, family status, military experience, cognitive style, and communication style. In a multi-perspective intellectual space, challenges to our beliefs and ideas are part of the learning process and can provide opportunities for growth. Reasoning, thoughtfulness, and open dialogues that honor the dignity of everyone is expected.  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
